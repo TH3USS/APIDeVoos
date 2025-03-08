3506cb76f7fbe12ee412580b8492c3074b2dcf63
Matheus <matheusramossilva2003@gmail.com>
2025-03-08 16:45:03 -0300

    refactor: Ajustes gerais no código

--- FlightBookingApi/FlightBookingApi/obj/Debug/net8.0/FlightBookingApi.AssemblyInfo.cs
+++ FlightBookingApi/FlightBookingApi/obj/Debug/net8.0/FlightBookingApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightBookingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23fdb1a6982262aa1e8524c8411208a331540749")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightBookingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightBookingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
